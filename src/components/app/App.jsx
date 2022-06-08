f2a1d129b56e0551d6551df0830032ce829f95f3
Florent Breux <breux.florent@gmail.com>
2022-06-08 12:42:41 +0200

    Config front with components files and styles

---
+++ src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import React from 'react';
+
+import logo from '/Users/florent/Desktop/projet-28-Ocrypto/src/assets/Logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
