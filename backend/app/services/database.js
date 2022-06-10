1a28130c9944c1f691186686b3772174c9df03e4
QuentinBouquet <quentin00bouquet@gmail.com>
2022-06-10 15:01:47 +0200

    Setup database module + auto launch of database

--- backend/app/services/database.js
+++ backend/app/services/database.js
@@ -1,20 +1,22 @@
 const { Pool } = require('pg');
-require('dotenv').config;
+require('dotenv').config();
 
-/*Create new pool*/
-/*Pool works like Client but it allow many connection in same time*/
-const client = new Pool({
-    user: process.env.DBUSER,
-    host: process.env.DBHOST,
-    database: process.env.DBNAME,
-    password: process.env.DBPASSORD,
-    port: process.env.DBPORT,
-});
+function lauchDB () {
+    /*Create new pool*/
+    /*Pool works like Client but it allow many connection in same time*/
+    const client = new Pool({
+        user:process.env.DBUSER,
+        host: process.env.DBHOST,
+        database:process.env.DBNAME,
+        password:process.env.DBPASSWORD,
+        port: process.env.DBPORT,
+    });
+    
+    client
+        .connect()
+        .then(()=>{console.log('Connection successful')})
+        .catch(error => console.log('Connection failed '+ error));
+};
 
-client
-    .connect()
-    .then(()=>{console.log('Connection successful')})
-    .catch(error => console.log('Connection failed : '+ error));
-
-module.exports = client;
+module.exports = {lauchDB};
 
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const server = require('./app/services/server');
+const database = require('./app/services/database');
 require('dotenv').config();
+
+/*Run the service database*/
+database.lauchDB();
 
 /* Run the service "server"*/
 const backServerPort = process.env.PORT || 3000;
