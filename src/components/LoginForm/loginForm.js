333a983dcc9a97e69c5903ac598157a2541b98bf
Matthieu-Simon <simon.matthieu89@gmail.com>
2022-06-13 14:32:04 +0200

    cssForm to finish and make the controlled components

--- src/components/LoginForm/loginForm.js
+++ src/components/LoginForm/loginForm.js
@@ -8,35 +8,33 @@ function LoginForm() {
         <h2>Inscription</h2>
         <form action="" method="get" className="form-inscription">
           <div className="form-example">
-            <input type="text" name="name" id="pseudo" placeholder="Pseudo" />
+            <input className="input-form" type="text" name="name" id="pseudo" placeholder="Pseudo" />
           </div>
           <div className="form-example">
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input className="input-form" type="email" name="email" id="email" placeholder="Email" />
           </div>
           <div className="form-example">
-            <input type="password" name="password" id="password" placeholder="Mot de passe" />
+            <input className="input-form" type="password" name="password" id="password" placeholder="Mot de passe" />
           </div>
           <div className="form-example">
-            <input type="password" name="password" id="confirm" placeholder="Confirmer mot de passe" />
+            <input className="input-form" type="password" name="password" id="confirm" placeholder="Confirmer mot de passe" />
           </div>
-          <div className="form-example">
-            <input type="submit" value="Valider" />
-          </div>
+          <button className="btn-form" type="submit">S'enregistrer</button>
         </form>
       </div>
       <span className="vertical" />
       <div className="register-right">
         <h2>Connexion</h2>
         <div className="form-example">
-          <input type="email" name="email" id="email" placeholder="Email" />
+          <input className="input-form" type="email" name="email" id="email" placeholder="Email" />
         </div>
         <div className="form-example">
-          <input type="password" name="password" id="password" placeholder="Mot de passe" />
+          <input className="input-form" type="password" name="password" id="password" placeholder="Mot de passe" />
         </div>
-        <button type="submit">Connexion</button>
+        <button className="btn-form" type="submit">Connexion</button>
         <div className="input-reset">
-          <input type="checkbox" id="memories" />
-          <label htmlFor="memories">Se souvenir de moi</label>
+          <input className="input-form" type="checkbox" id="memories" />
+          <a href="#">Se souvenir de moi</a>
           <a href="#">Mot de passe oublié ?</a>
         </div>
       </div>
