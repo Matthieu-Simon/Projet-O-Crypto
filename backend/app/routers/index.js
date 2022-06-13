c7d95dc5a6ed739fa25f92eae76733fa2a116533
QuentinBouquet <quentin00bouquet@gmail.com>
2022-06-13 13:47:31 +0200

    Add homepage route

--- backend/app/routers/index.js
+++ backend/app/routers/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const userRouter = require('./user');
+const homepageRouter = require('./homepage');
 
+router.use(homepageRouter);
 router.use(userRouter);
 
 module.exports = router;
