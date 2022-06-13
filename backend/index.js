37047afc4149f6afe23ac01cbe325c611cec5391
QuentinBouquet <quentin00bouquet@gmail.com>
2022-06-13 10:46:02 +0200

    Add routes : user mofification (not working yet)

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,9 @@ const server = require('./app/services/server');
 const database = require('./app/services/database');
 require('dotenv').config();
 
-/*Run the service database*/
-database.lauchDB();
+
 
 /* Run the service "server"*/
 const backServerPort = process.env.PORT || 3000;
 const backServerMessage = 'Hello O Crypto'
 server.initServer(backServerPort, backServerMessage);
-
