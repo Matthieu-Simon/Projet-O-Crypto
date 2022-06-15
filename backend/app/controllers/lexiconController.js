2daaf8dc5472436398290ce8ca8a3af78e66fe71
HugoB <ugobrancatelli@gmail.com>
2022-06-15 16:34:56 +0200

    Edit lexicon and test with thunder client+ add seeding artcile

--- backend/app/controllers/lexiconController.js
+++ backend/app/controllers/lexiconController.js
@@ -1,13 +1,12 @@
 const lexiconDatamapper = require('../dataMappers/lexiconDataMapper');
 
 module.exports = {
-    async getAlllexicon(request, response) {
-        const getAlllexicon = await lexiconDatamapper.getListwordDefinition();
-        return response.json(getAlllexicon);
+    async getAllLexicon(request, response) {
+        const getAllLexicon = await lexiconDatamapper.getListwordDefinition();
+        return response.json(getAllLexicon);
     },
-
     async getOneWordOfLexicon(request, response) {
-        const word = request.body;
+        const word = request.params.name;
         const getOneLexicon = await lexiconDatamapper.getOneWordDefinition(word);
         return response.json(getOneLexicon);
     },
