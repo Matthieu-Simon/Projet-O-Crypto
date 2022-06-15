0707a48b8363a02b54c3725f66fcd38e73e097e1
HugoB <ugobrancatelli@gmail.com>
2022-06-15 16:40:37 +0200

    Revert "Revert "Edit lexicon and test with thunder client+ add seeding artcile""
    This reverts commit 2f96a4a2f68cd6952cc28d48d3ddbfaeca5377ab.

--- backend/app/routers/lexicon.js
+++ backend/app/routers/lexicon.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const lexiconController = require('../controllers/lexiconController');
 
 /* Route to get all article */
-router.route('/lexicon').get(lexiconController.getAlllexicon);
+router.route('/lexicon').get(lexiconController.getAllLexicon);
 
 /* Route to get one article */
-router.route('/lexicon/:word').get(lexiconController.getOneWordOfLexicon);
+router.route('/lexicon/:name').get(lexiconController.getOneWordOfLexicon);
 
 module.exports = router;
