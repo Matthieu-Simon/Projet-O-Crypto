49e173524769387182961eb64e71a9c423f15232
Florent Breux <breux.florent@gmail.com>
2022-06-20 18:22:40 +0200

    Added darkmode switch, TODO style for darkmode

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
 import './App.scss';
+import { createContext, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ReactSwitch from 'react-switch';
 import authService from '../LoginForm/auth.service';
 
 import Header from '../header/header';
@@ -20,33 +23,46 @@ import About from '../About/about';
 import OurTeam from '../OurTeam/ourTeam';
 import PageNotFound from '../404/PageNotFound';
 
+export const Themecontext = createContext(null);
+
 function App() {
   const isLogged = authService.getCurrentUser();
+  const [theme, setTheme] = useState('light');
+
+  const toogleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
+  };
 
   return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {isLogged && (<Route path="/profile" element={<Profil />} />)}
-        <Route path="/dashboard" element={<DashBoard />} />
-        <Route path="/coin/:id" element={<CoinPage />} />
-        <Route path="/log-in" element={<LoginForm />} />
-        <Route path="/learning" element={<LearningJourney />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/Cours" element={<Cours />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/team" element={<OurTeam />} />
-        <Route path="challenge" element={<Challenge />} />
-        <Route path="/team" element={<OurTeam />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/articles/:name" element={<Article />} />
-        <Route path="/lexicon" element={<Lexicon />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer />
-    </div>
+    <Themecontext.Provider value={{ theme, toogleTheme }}>
+      <div className="App" id={theme}>
+        <Header />
+        <div className="switch-container">
+          <label> {theme === 'light' ? 'Mode Clair' : 'Mode Sombre'}</label>
+          <ReactSwitch onChange={toogleTheme} checked={theme === 'dark'} offColor="#424C7C" onColor="#FFFFFF" onHandleColor="#424C7C" />
+        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {isLogged && (<Route path="/profile" element={<Profil />} />)}
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/coin/:id" element={<CoinPage />} />
+          <Route path="/log-in" element={<LoginForm />} />
+          <Route path="/learning" element={<LearningJourney />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/Cours" element={<Cours />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/team" element={<OurTeam />} />
+          <Route path="challenge" element={<Challenge />} />
+          <Route path="/team" element={<OurTeam />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles/:name" element={<Article />} />
+          <Route path="/lexicon" element={<Lexicon />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Themecontext.Provider>
   );
 }
 export default App;
