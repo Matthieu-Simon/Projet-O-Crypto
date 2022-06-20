6cd7a22b6f71ceacc19951fc21f315fea24eb2c9
Florent Breux <breux.florent@gmail.com>
2022-06-20 16:48:16 +0200

    Responsive LearningJourney

--- src/components/DashBoard/CoinPage/CoinChart/SelectedButton/selectedButton.js
+++ src/components/DashBoard/CoinPage/CoinChart/SelectedButton/selectedButton.js
@@ -4,7 +4,7 @@ import './selectedButtonStyles.scss';
 function SelectButton({ children, onClick }) {
   return (
     <span onClick={onClick} className="selected-button">
-      {children.toLowerCase()}
+      {children}
     </span>
   );
 }
