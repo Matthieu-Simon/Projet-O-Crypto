d4456f7045d3df60d8cd718dcfad55da771285f2
QuentinBouquet <quentin00bouquet@gmail.com>
2022-06-21 16:19:40 +0200

    Add reward calculation and level evolution

--- backend/app/controllers/rewardController.js
+++ backend/app/controllers/rewardController.js
@@ -1,10 +1,37 @@
 const rewardDataMapper = require('../dataMappers/rewardDatamapper');
 
+
 module.exports = {
     async goodAnwserChecking (request, response) {
         const questionId = request.params.question_id;
         const checking = request.body;       
         const goodAnswerChecking = await rewardDataMapper.goodAnswerChecking(questionId, checking);
         response.json(goodAnswerChecking);
-    }
+
+        if (goodAnswerChecking === true) {
+            const getUserReward = await rewardDataMapper.getUserRewardAndLevel(4);
+
+            const getQuestionRewardById = await rewardDataMapper.getQuestionRewardById(questionId);
+            
+            const newUserReward = getUserReward.reward + getQuestionRewardById.reward
+
+            await rewardDataMapper.insertReward(newUserReward, 4);
+
+            const levels = ['Novice','Confirmé', 'Expert', 'Tech Fibre'];
+    
+            if (newUserReward>0 && newUserReward<299) { 
+                await rewardDataMapper.insertLevel(levels[0], 4);
+    
+            } else if (newUserReward>299 && newUserReward<899) {
+                await rewardDataMapper.insertLevel(levels[1], 4);
+    
+            } else if (newUserReward>899 && newUserReward<1199) {
+                await rewardDataMapper.insertLevel(levels[2], 4);
+    
+            } else if (newUserReward>1199 && newUserReward<999999999) {
+                await rewardDataMapper.insertLevel(levels[3], 4);
+            }
+        }
+
+    },    
 };
--- backend/app/dataMappers/rewardDatamapper.js
+++ backend/app/dataMappers/rewardDatamapper.js
@@ -2,7 +2,6 @@ const client = require('../services/database');
 
 module.exports = {
     async goodAnswerChecking (question_id, checking) {
-        console.log(checking)
         const goodAnswerChecking = await client.query(`
             SELECT answer.id, answer.description, answer.question_id
             FROM answer, question
@@ -10,10 +9,43 @@ module.exports = {
             AND question.id = ${question_id};
         `);
         if (checking.id !== goodAnswerChecking.rows[0].id) {
-            return 'There is the wrong answer'
+            return false;
         }
+        return true;
+    },
 
-        return 'There is the good answer GG';
+    async getUserRewardAndLevel(user_id) {
+        const getUserRewardById = await client.query(`
+            SELECT "user".pseudo, "user".reward, "user".level
+            FROM "user"
+            WHERE id= ${user_id};
+        `);
+        return getUserRewardById.rows[0];
+    },
 
+    async getQuestionRewardById (question_id) {
+        const getQuestionRewardById = await client.query(`
+            SELECT question.id, question.description, question.reward
+            FROM question
+            WHERE id= ${question_id};
+        `);
+        return getQuestionRewardById.rows[0]
+    },
+
+    async insertReward (newUserReward, user_id) {
+        const insertReward = await client.query(`
+            UPDATE "user"
+            SET reward= ${newUserReward}
+            WHERE id= ${user_id};
+        `);
+    },
+
+    async insertLevel (newUserLevel, user_id) {
+        console.log(newUserLevel)
+        const insertLevel = await client.query(`
+            UPDATE "user"
+            SET level= '${newUserLevel}'
+            WHERE id= ${user_id};
+        `);
     }
 }
