abad9dc95475171e57a6012a501fcbcc2cc8d843
HugoB <ugobrancatelli@gmail.com>
2022-06-22 12:57:13 +0200

    edit favoris and test route

--- backend/app/routers/crypto.js
+++ backend/app/routers/crypto.js
@@ -6,7 +6,7 @@ const cryptoController = require('../controllers/cryptoController');
 
 /* Route to home page */
 // router.route('/favoris/:userId').post(cryptoController.addOneCryptoFavoris);
-router.route('/favoris/userId/cryptos').get(cryptoController.getListCryptoFavoris);
+router.route('/favoris/:userId/cryptos').get(cryptoController.getListCryptoFavoris);
 router.route('/favoris/:userId/:cryptos').post(cryptoController.addOneCryptoFavoris);
 router.route('/deleteFavoris/:userId/:cryptos').post(cryptoController.deleteOneCryptoFavoris);
 
