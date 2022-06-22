247a76d181a662f3dac9ba8c2ab0730311611465
HugoB <ugobrancatelli@gmail.com>
2022-06-22 18:22:08 +0200

    Edit crypto

--- backend/app/controllers/recoveryPasswordController.js
+++ backend/app/controllers/recoveryPasswordController.js
@@ -5,13 +5,13 @@ const nodemailer = require('../services/nodeMailer');
 const newRandomPassword = randomPassword.toString();
 
 module.exports = {
-    async insertTemporaryPassword (request, response) {
+    async insertTemporaryPassword(request, response) {
         const userEmail = request.body.email;
         const setTemporaryPassword = await recoveryPasswordDataMapper.recoveryPassword(newRandomPassword, userEmail);
 
-        /*Send an email to the user*/
+        /* Send an email to the user */
         nodemailer(userEmail, newRandomPassword);
 
-        return response.json(setTemporaryPassword)
-    }
-}
+        return response.json(setTemporaryPassword);
+    },
+};
