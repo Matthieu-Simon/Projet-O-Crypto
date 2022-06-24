7810f7425c2b3f39677deb49f992dce9d8632ef6
QuentinBouquet <quentin00bouquet@gmail.com>
2022-06-24 16:18:29 +0200

    Reward logic modification for front

--- backend/app/routers/reward.js
+++ backend/app/routers/reward.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const rewardController = require('../controllers/rewardController')
 
 /*Route to check the answer*/
-router.route('/answer/checking/:question_id').get(rewardController.goodAnwserChecking);
+router.route('/answer/checking/:user_id/:question_id')
+    .get(rewardController.goodAnwserChecking)
+    .post(rewardController.goodAnwserChecking);
 
 module.exports = router;
